61534e529631852db26d1ae94b2ac72e607ac22d
fyosu <josuahabeahan2016@gmail.com>
2024-12-13 17:22:25 +0700

    NewRealese

--- app/src/main/java/com/capstone_bangkit/nutrise/ui/setting/history/HistoryAdapter.kt
+++ app/src/main/java/com/capstone_bangkit/nutrise/ui/setting/history/HistoryAdapter.kt
@@ -1,7 +1,7 @@
 package com.capstone_bangkit.nutrise.ui.setting.history
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -10,12 +10,12 @@ import com.capstone_bangkit.nutrise.database.History
 import com.capstone_bangkit.nutrise.databinding.ItemHistoryBinding
 
 class HistoryAdapter(
-    private var historyList: List<History>
+    private var historyList : List<History>
 ) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
 
-    inner class HistoryViewHolder(private val binding: ItemHistoryBinding) :
+    inner class HistoryViewHolder(private val binding : ItemHistoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(history: History) {
+        fun bind(history : History) {
             with(binding) {
                 tvResult.text = history.result
                 tvDate.text = history.date
@@ -27,7 +27,7 @@ class HistoryAdapter(
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : HistoryViewHolder {
         val binding = ItemHistoryBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -36,14 +36,15 @@ class HistoryAdapter(
         return HistoryViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+    override fun onBindViewHolder(holder : HistoryViewHolder, position : Int) {
         holder.bind(historyList[position])
     }
 
-    override fun getItemCount(): Int = historyList.size
+    override fun getItemCount() : Int = historyList.size
 
     // Update data adapter
-    fun updateData(newList: List<History>) {
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateData(newList : List<History>) {
         historyList = newList
         notifyDataSetChanged()  // Notify adapter that data has changed
     }
