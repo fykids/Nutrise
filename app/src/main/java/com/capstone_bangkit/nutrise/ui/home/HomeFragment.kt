4b99281737598366ba1c72cc50724977d1e8023f
fyosu <josuahabeahan2016@gmail.com>
2024-12-13 11:50:45 +0700

    NewRealese

--- app/src/main/java/com/capstone_bangkit/nutrise/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone_bangkit/nutrise/ui/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.capstone_bangkit.nutrise.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.capstone_bangkit.nutrise.databinding.FragmentHomeBinding
+import com.capstone_bangkit.nutrise.ui.home.camera.CameraActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +23,17 @@ class HomeFragment : Fragment() {
     ) : View {
         _binding = FragmentHomeBinding.inflate(LayoutInflater.from(context), container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentUser = Firebase.auth.currentUser
+        binding.nameUser.text = currentUser?.displayName.toString()
+
+        binding.buttonStart.setOnClickListener {
+            startActivity(Intent(requireContext(), CameraActivity::class.java))
+        }
     }
 
     override fun onDestroyView() {
